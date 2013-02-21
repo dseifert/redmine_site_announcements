097e73c490d08be387edb3e925d4df7f30160f83
Greg Thornton <xdissent@me.com>
2013-02-21 01:29:15 -0600

    Require Redmine 2.2.3

--- init.rb
+++ init.rb
@@ -7,6 +7,7 @@ Redmine::Plugin.register :redmine_site_announcements do
   version "0.0.1"
   url "http://github.com/xdissent/redmine_site_announcements"
   author_url "http://xdissent.com"
+  requires_redmine version_or_higher: "2.2.3"
 
   # Add announcements link to admin menu.
   menu :admin_menu, :announcements, 
